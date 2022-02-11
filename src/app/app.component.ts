28374c8425f20c6a12a3396e7b013eef300b9afe
Augustin Dinulica <dinulicaaugustin@gmail.com>
2022-02-11 15:40:03 +0200

    cat a trecut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { differenceInMinutes } from 'date-fns';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'cat_a_mai_trecut_din_iarna';
+export class AppComponent implements OnInit {
+  startDate = new Date('2021-12-01T00:00:00');
+  endDate = new Date('2022-03-01T00:00:00');
+  totalWinterTimeInMinutes: number = 0;
+  passedWinterTimeInMinutes: number = 0;
+  percentagePassed = '';
+
+  ngOnInit() {
+    this.totalWinterTimeInMinutes = differenceInMinutes(this.startDate, this.endDate);
+    setInterval(() => {
+      const currentDate = new Date();
+      this.passedWinterTimeInMinutes = differenceInMinutes(this.startDate, currentDate);
+      this.percentagePassed = `${((this.passedWinterTimeInMinutes * 100) / this.totalWinterTimeInMinutes).toFixed(2)} %`;
+    }, 1000);
+  }
 }

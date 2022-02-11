23e22a25f294ed6fab352114936637a2abd59428
Augustin Dinulica <dinulicaaugustin@gmail.com>
2022-02-11 14:29:22 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'cat_a_mai_trecut_din_iarna';
+}
